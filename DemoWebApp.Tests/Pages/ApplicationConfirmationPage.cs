9b70b8372831d3ec404df78d0744579e613d6832
Andrei Nicolescu <Andrei.Nicolescu@sparkware.ro>
2019-04-15 10:43:19 +0300

    Converted Confirmation POM not to use PageFactory

--- DemoWebApp.Tests/Pages/ApplicationConfirmationPage.cs
+++ DemoWebApp.Tests/Pages/ApplicationConfirmationPage.cs
@@ -12,14 +12,11 @@ namespace DemoWebApp.Tests
     {
         private readonly IWebDriver _driver;
 
-        [FindsBy(How = How.Id, Using = "firstName")]
-        private IWebElement _firstName;
+        private IWebElement _firstName => _driver.FindElement(By.Id("firstName"));
 
         public ApplicationConfirmationPage(IWebDriver driver)
         {
             _driver = driver;
-
-            PageFactory.InitElements(_driver, this);
         }
 
         public string FirstName => _firstName.Text;
